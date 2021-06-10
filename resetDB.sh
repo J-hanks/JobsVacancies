01069b39eb0ccdb45a82888460ed0e2530105bcc
Joao Victor Soares da Cunha <jvsdc1992@gmail.com>
2021-06-10 17:37:58 -0300

    reste db tool

---
+++ resetDB.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+rm db.sqlite3
+python manage.py makemigrations
+python manage.py migrate
+
+
+python manage.py loaddata "Companies.json"
+python manage.py loaddata "JobsVacancies.json"
+python manage.py loaddata "WebUser.json"
+python manage.py loaddata "WebUserAdm.json"
+python manage.py loaddata "WebUserCompany.json"
+python manage.py loaddata "JobsApplications.json"
+
+# python manage.py loaddata "MerchantJoao.json"
